22d2fb6e0cf19e80a4c6bc8e8a7428717b389b3f
Ritam <ritamdevs2004@gmail.com>
2025-06-12 00:19:43 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import CompanionCard from "@/components/CompanionCard";
 import CompanionList from "@/components/CompanionsList";
 import Cta from "@/components/Cta";
